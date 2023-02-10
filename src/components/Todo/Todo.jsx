0a95a477a2d6ae8eb028245bfc894dd6984b57a7
ClownNero <ClownNero@gmail.com>
2023-02-10 16:15:11 +0900

    First

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {BsTrashFill} from 'react-icons/bs';
+import styles from './Todo.module.css';
+
+export default function Todo({todo, onUpdate, onDelete}) {
+    
+    const {text, status} = todo;
+    const handleChange = (e)=>{
+        const status = e.target.checked? 'completed' : 'active';
+        onUpdate({...todo, status: status});
+    };
+    const handleDelete = () => onDelete(todo);
+    return <li className={styles.todo}>
+        <input
+            className={styles.checkbox}
+            type='checkbox' 
+            id={todo.id}
+            checked ={status ==='completed'}
+            onChange={handleChange}
+        />
+        <label htmlFor={todo.id} className={styles.text}>{text}</label>
+        <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
+            <BsTrashFill />
+            </button>
+        </span>
+    </li>
+}
+
